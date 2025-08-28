21ce0c8608a5e6a86853851a90c9354d49cbb983
farial-robama <farialrobama15@gmail.com>
2025-08-28 17:05:49 +0200

    correct event listener for heart, call & clear history buttons

--- script.js
+++ script.js
@@ -34,36 +34,37 @@ function renderHistory() {
     for (const item of historyData) {
         const div = document.createElement("div")
         div.innerHTML = `
-        <div class="card bg-[#FAFAFA] card-xs shadow-sm flex flex-row gap-5 items-center px-3 py-2 mt-3">
+        <div class="card bg-[#FAFAFA] card-xs shadow-sm flex flex-row gap-4 items-center px-3 py-2 mt-3">
            <div class="card-body flex flex-col">
                 <h2 class="card-title">${item.name}</h2>
                 <p>${item.number}</p>
             </div>
-            <p>${item.time}</p>
+            <p class="whitespace-nowrap">${item.time}</p>
         </div>`
 
         historyList.appendChild(div)
     }
 }
 
+// heart-btn
 document.querySelectorAll(".heart-btn").forEach((btn) => {
-    btn.addEventListener("click",function () {
+    btn.addEventListener("click",function() {
         hearts++;
         updateCounters();
     })
 });
 
-document.querySelectorAll(".call-btn").forEach((btn) => {
-    btn.addEventListener("click",function () {
+// call-btn
+document.querySelectorAll(".heart-btn").forEach((btn) => {
+    btn.addEventListener("click",function() {
         const card = btn.closest(".card");
         const name = card.dataset.serviceName;
         const number = card.dataset.serviceNumber;
-
+        
         if (coins < 20) {
         alert("Not enough coins!");
         return
     }
-
     coins -= 20;
     updateCounters();
     alert(`Calling ${name} (${number})`);
@@ -72,10 +73,11 @@ document.querySelectorAll(".call-btn").forEach((btn) => {
     });
 });
 
+// clear-history-btn
 document.getElementById("clear-history-btn").addEventListener("click",function(){
     historyData.length = 0;
     renderHistory()
 })
-
 updateCounters();
+
 
